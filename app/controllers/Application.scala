e9c971d42c4f036bc93c2e5e3ffd3793606a59a9
Ikenna Nwaiwu <ikenna.nwaiwu@guardian.co.uk>
2014-04-11 12:18:16 +0100

    Refactoring

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,11 +8,25 @@ import com.typesafe.config.ConfigFactory
 
 object Application extends Controller {
 
-
+  /*
+   Simple page that presents the subscription link at
+  http://localhost:9000/
+  * */
   def index = Action {implicit request =>
     Ok(views.html.index(subscriptionUrl))
   }
 
+  def subscriptionUrl(implicit request: Request[AnyContent]): String = {
+    val (amount, intervalLength, intervalUnit) = (new math.BigDecimal("15.0"), 1, "month")
+    val subscription = new Subscription(ApiKeys.merchantId, amount, intervalLength, intervalUnit)
+    val redirectUri = routes.Application.confirm().absoluteURL()
+    Account.connect.newSubscriptionUrl(subscription, redirectUri, null, null)
+  }
+
+  /* http://localhost:9000/confirm
+  After the user clicks the subscription link and keys their bank details into the GoCardless API page, the user
+   is redirected to this confirm page, which sends a confirmation to GoCardless
+  * */
   def confirm = Action { request =>
     val resource: Option[Resource] = for {
       resourceUri <- request.getQueryString("resource_uri")
@@ -27,20 +41,10 @@ object Application extends Controller {
       r.setSignature(signature)
       r
     }
-
     resource map Account.connect.confirm
     Ok("Your Direct Debit Subscription has been setup. Thank you for setting up a direct debit!")
   }
-
-
-  def subscriptionUrl(implicit request: Request[AnyContent]): String = {
-    val (amount, intervalLength, intervalUnit) = (new math.BigDecimal("15.0"), 1, "month")
-    val subscription = new Subscription(ApiKeys.merchantId, amount, intervalLength, intervalUnit)
-    val redirectUri = routes.Application.confirm().absoluteURL()
-    Account.connect.newSubscriptionUrl(subscription, redirectUri, null, null)
-  }
 }
-
 
 object ApiKeys {
   val conf = ConfigFactory.systemEnvironment()
